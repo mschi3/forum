f7fe3e43260045efdf7868f6e369c542c42a8b3c
Cheryll <crvaldez@gmail.com>
2016-11-12 18:58:02 +0800

    include comment view, category model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(comment_params)
+		@comment.user_id = current_user.id if current_user
+		@comment.save
+
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render :action => :new
+		end
+	end
+
+	private
+
+	def comment_params
+		params[:comment].permit(:comment)
+	end
+
 end
+
