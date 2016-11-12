0878ed8fd3e4849419e2d4dd4b2eedf29958a5ea
Cheryll <crvaldez@gmail.com>
2016-11-12 19:15:12 +0800

    include edit, update, destroy for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,37 @@ class CommentsController < ApplicationController
 		@comment.save
 
 		if @comment.save
+			flash[:notice] = "Comment was successfully created"
 			redirect_to post_path(@post)
 		else
 			render :action => :new
 		end
 	end
 
+	def edit
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+
+		if @comment.update(comment_params)
+			flash[:notice] = "Comment was successfully updated"
+			redirect_to post_path(@post)
+		else
+			render :action => :edit
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		flash[:alert] = "Comment was successfully deleted"
+		redirect_to post_path(@post)
+	end
 	private
 
 	def comment_params
