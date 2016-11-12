dde3ba5d57fb2cd148bea63745a941e346eb3879
Cheryll <crvaldez@gmail.com>
2016-11-12 09:38:06 +0800

    inlcude edit,update and destroy and update view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,28 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			flash[:notice] = "Post was successfully created"
+			redirect_to @post
+		else
+			render :action => :edit
+		end
+	end
+
+	def destroy
+ 		@post = Post.find(params[:id])
+ 		@post.destroy
+ 		flash[:alert] = "Post was successfully deleted"
+ 		redirect_to :action => :index
+ 	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :post)
+	end
+
 end
