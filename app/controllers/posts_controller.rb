177d47091ad9caf26c34d2c2bee3785ed890b2a3
Cheryll <crvaldez@gmail.com>
2016-11-12 08:43:49 +0800

    setup table, update controller and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
 	def index
+		@posts = Post.all
   end
+
+  def show
+  	@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
 end
